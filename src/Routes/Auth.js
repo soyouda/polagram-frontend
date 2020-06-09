56ce0d65663561cd99799d59f0a248bd1c2eec17
soyouda <soyouda@gmail.com>
2020-06-09 19:13:14 +0900

    5.2 Footer and Auth UI 2

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -9,7 +9,18 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled.div`
-  ${(props) => props.theme.whiteBox};
+  ${(props) => props.theme.whiteBox} border-radius:0px;
+  width: 350px;
+`;
+
+const StateChanger = styled(Box)`
+  text-align: center;
+  padding: 20px 0px;
+`;
+
+const Link = styled.span`
+  color: ${(props) => props.theme.blueColor};
+  cursor: pointer;
 `;
 
 export default () => {
@@ -17,7 +28,19 @@ export default () => {
 
   return (
     <Wrapper>
-      {action === "logIn" ? <Box>Log In</Box> : <Box>Sign In</Box>}
+      <StateChanger>
+        {action === "logIn" ? (
+          <>
+            Don't have an account?{" "}
+            <Link onClick={() => setAction("signUp")}>Sign up</Link>
+          </>
+        ) : (
+          <>
+            Have an account?{" "}
+            <Link onClick={() => setAction("logIn")}>Log in</Link>
+          </>
+        )}
+      </StateChanger>
     </Wrapper>
   );
 };
